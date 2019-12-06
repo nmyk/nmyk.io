3662cc01c9b53e3e928bfc3545afb3488166a3c2
Nick Mykins <nmykins@digitalocean.com>
2019-12-06 08:30:57 -0500

    write little go app that serves index.html, dockerize

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "web/index.html")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
