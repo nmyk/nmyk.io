190f5d66fcf2ee167700ee7c94d68971f8491fed
Nick Mykins <nmykins@digitalocean.com>
2020-01-26 12:44:29 -0500

    rm some comments

--- pkg/main.go
+++ pkg/main.go
@@ -21,16 +21,8 @@ type IndexData struct {
 func main() {
 	tmpl := template.Must(template.ParseFiles("web/templates/index.gohtml"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			// Set params for the color-changing background here rather than
-			// on the client so we don't have to see a split second of wrong
-			// background before the body element's `.onload` event handler
-			// updates the element's `animation-delay` style property.
 			d := IndexData{
 				BgAnimationDuration: bgAnimationDurationSeconds,
-				// Setting `animation-delay` to -X seconds begins the animation
-				// as though it had already been playing for X seconds. We use
-				// a function of time.Now() to make it so that everyone sees the
-				// same thing, as though it were a natural phenomenon.
 				BgAnimationDelay: int(-time.Now().Unix() % bgAnimationDurationSeconds),
 			}
 			tmpl.Execute(w, d)
