2b5fe42cc8f364f3e17a5db8fc5b25056c0d191d
Nick Mykins <nmykins@digitalocean.com>
2020-01-26 12:34:22 -0500

    make animation duration value only appear in one place

--- pkg/main.go
+++ pkg/main.go
@@ -1,28 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
 )
 
-type IndexData struct {
-	AnimationDelay string
-}
+// bgAnimationDurationSeconds is how long a full cycle of the color-changing
+// background animation will take. It should be short enough that folks will
+// eventually notice the colors changing, but long enough so that when they
+// notice, it feels like discovering a hidden treat.
+const bgAnimationDurationSeconds = 400
 
-func getAnimationDelay() string {
-	return fmt.Sprintf("%ds", -(time.Now().Unix() % 400))
+type IndexData struct {
+	BgAnimationDuration int
+	BgAnimationDelay int
 }
 
 func main() {
-	tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
+	tmpl := template.Must(template.ParseFiles("web/templates/index.gohtml"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			data := IndexData{
-				AnimationDelay: getAnimationDelay(),
+			// Set params for the color-changing background here rather than
+			// on the client so we don't have to see a split second of wrong
+			// background before the body element's `.onload` event handler
+			// updates the element's `animation-delay` style property.
+			d := IndexData{
+				BgAnimationDuration: bgAnimationDurationSeconds,
+				// Setting `animation-delay` to -X seconds begins the animation
+				// as though it had already been playing for X seconds. We use
+				// a function of time.Now() to make it so that everyone sees the
+				// same thing, as though it were a natural phenomenon.
+				BgAnimationDelay: int(-time.Now().Unix() % bgAnimationDurationSeconds),
 			}
-			tmpl.Execute(w, data)
+			tmpl.Execute(w, d)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
